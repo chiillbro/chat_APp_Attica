94617f1300472c59c2f6a98ed7382d8470268f20
Siva Sankar Reddy <aasamsivasankarreddynani143@gmail.com>
2024-06-08 16:50:38 +0530

    base url

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,2 +1,2 @@
-export const BASE_URL = "http://localhost:5001"
-export const emplyeRegistration = `${BASE_URL}/api/employeeRegistration/register` 
+export const BASE_URL = "https://chat-app-attica.onrender.com";
+export const emplyeRegistration = `${BASE_URL}/api/employeeRegistration/register`;
